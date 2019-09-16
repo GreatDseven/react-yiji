484d8a4ea63440658dcf40d7283b0ef7139da4e1
GreatDseven <582858152@qq.com>
2019-09-16 15:11:32 +0800

    09161511

---
+++ precache-manifest.75cbdf5c9523ac5db35cf0d671311fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f1e52b8d7b1433c7bd146f8b3fbec6",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "2908143fc208b51d5a30",
+    "url": "/react-yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "c0abaddfc4d9ec74ce80",
+    "url": "/react-yiji/static/css/main.4db7104c.chunk.css"
+  },
+  {
+    "revision": "2908143fc208b51d5a30",
+    "url": "/react-yiji/static/js/2.d41a20bd.chunk.js"
+  },
+  {
+    "revision": "c0abaddfc4d9ec74ce80",
+    "url": "/react-yiji/static/js/main.ec488326.chunk.js"
+  },
+  {
+    "revision": "76e3e25619ead56721a2",
+    "url": "/react-yiji/static/js/runtime~main.df17a495.js"
+  }
+]);
--- src/store/selectors.js
+++
@@ -1,3 +0,0 @@
-export const isGood = (show, id) => {
-  return show.find(item => item.id === id).isGood.find(item => item === "myid");
-};
